080eca91ae61cee7383ec72d841e9765b80151d7
Alex Mitchell <Alex.Mitchell@labor.idaho.gov>
2016-04-21 16:36:58 -0600

    Fixed tests

--- tests/WebApi.Hal.Tests/HalResourceListTests.cs
+++ tests/WebApi.Hal.Tests/HalResourceListTests.cs
@@ -1,10 +1,25 @@
-﻿using ApprovalTests;
+﻿using System;
+using ApprovalTests;
 using ApprovalTests.Reporters;
 using iUS.WebApi.Hal;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Http.Internal;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.Abstractions;
+using Microsoft.AspNet.Mvc.Formatters;
+using Microsoft.AspNet.Mvc.Infrastructure;
+using Microsoft.AspNet.Routing;
+using Microsoft.Extensions.Primitives;
+using Microsoft.Net.Http.Headers;
+using Moq;
+using Newtonsoft.Json;
 using WebApi.Hal.Tests.Representations;
 using Xunit;
+
 
 namespace WebApi.Hal.Tests
 {
@@ -32,59 +47,79 @@ namespace WebApi.Hal.Tests
 
         [Fact]
         [UseReporter(typeof(DiffReporter))]
-        public void organisation_list_get_xml_test()
+        public async Task organisation_list_get_xml_test()
         {
             // arrange
             var mediaFormatter = new XmlHalMediaTypeOutputFormatter();
+            
+            var context = HalResourceTest.GetOutputFormatterContext(representation, typeof(OrganisationListRepresentation));
 
             // act
-            using (var stream = new StringWriter())
-            {
-                mediaFormatter.WriteObject(stream, representation);
+            await mediaFormatter.WriteResponseBodyAsync(context);
 
-                string serialisedResult = stream.ToString();
+            var body = context.HttpContext.Response.Body;
 
-                // assert
-                Approvals.Verify(serialisedResult);
-            }
+            Assert.NotNull(body);
+            body.Position = 0;
+
+            var content = new StreamReader(body, Encoding.UTF8).ReadToEnd();
+
+            // assert
+            Approvals.Verify(content);
         }
 
         [Fact]
         [UseReporter(typeof(DiffReporter))]
-        public void organisation_list_get_json_test()
+        public async Task organisation_list_get_json_test()
         {
             // arrange
-            var mediaFormatter = new JsonHalMediaTypeOutputFormatter();
+            var mediaFormatter = new JsonHalMediaTypeOutputFormatter()
+            {
+                SerializerSettings = { Formatting = Formatting.Indented}
+            };
+
+            var context = HalResourceTest.GetOutputFormatterContext(representation, typeof(OrganisationListRepresentation));
 
             // act
-            using (var stream = new StringWriter())
-            {
-                mediaFormatter.WriteObject(stream, representation);
+            await mediaFormatter.WriteResponseBodyAsync(context);
 
-                string serialisedResult = stream.ToString();
+            var body = context.HttpContext.Response.Body;
 
-                // assert
-                Approvals.Verify(serialisedResult);
-            }
+            Assert.NotNull(body);
+            body.Position = 0;
+
+            var content = new StreamReader(body, Encoding.UTF8).ReadToEnd();
+
+            // assert
+            Approvals.Verify(content);
         }
 
         [Fact]
         [UseReporter(typeof(DiffReporter))]
-        public void one_item_organisation_list_get_json_test()
+        public async Task one_item_organisation_list_get_json_test()
         {
             // arrange
-            var mediaFormatter = new JsonHalMediaTypeOutputFormatter();
+            var mediaFormatter = new JsonHalMediaTypeOutputFormatter
+            {
+                SerializerSettings = {Formatting = Formatting.Indented}
+            };
+
+            var context = HalResourceTest.GetOutputFormatterContext(oneitemrepresentation, typeof(OrganisationListRepresentation));
 
             // act
-            using (var stream = new StringWriter())
-            {
-                mediaFormatter.WriteObject(stream, oneitemrepresentation);
+            
+            await mediaFormatter.WriteResponseBodyAsync(context);
 
-                string serialisedResult = stream.ToString();
+            var body = context.HttpContext.Response.Body;
 
-                // assert
-                Approvals.Verify(serialisedResult);
-            }
+            Assert.NotNull(body);
+            body.Position = 0;
+
+            var content = new StreamReader(body, Encoding.UTF8).ReadToEnd();
+
+            // assert
+            Approvals.Verify(content);
+            
         }
     }
 }
